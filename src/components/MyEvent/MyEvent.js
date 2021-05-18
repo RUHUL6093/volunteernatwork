c18313592a3441816aea42fcf9ec7016af42518d
RUHUL AMIN <59334063+RUHUL6093@users.noreply.github.com>
2021-05-18 23:21:38 +0600

    register ebevt

---
+++ src/components/MyEvent/MyEvent.js
@@ -0,0 +1,9 @@
+import React from "react";
+const MyEvent = () => {
+  return (
+    <div>
+      <h1>This ismyevent</h1>
+    </div>
+  );
+};
+export default MyEvent;
---
+++ src/components/RegisterEvent/RegisterEvent.js
@@ -0,0 +1,9 @@
+import React from "react";
+const RegisterEvent = () => {
+  return (
+    <div>
+      <h1>This is destination</h1>
+    </div>
+  );
+};
+export default RegisterEvent;
