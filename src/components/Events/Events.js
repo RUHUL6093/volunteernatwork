c242a21715de6b8b5402343817c7881dc4fc8d6a
RUHUL AMIN <59334063+RUHUL6093@users.noreply.github.com>
2021-05-19 23:07:45 +0600

    ok some hing

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -3,9 +3,20 @@ import fakedata from "../FakeData/fakeData.json";
 import Event from "../Event/Event";
 const Events = () => {
   // console.log(fakedata);
+  const handleAllEvents = () => {
+    fetch("https://9wtjs.sse.codesandbox.io/addEvents", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(fakedata)
+    });
+  };
+
   return (
     <div className="row">
-    { fakedata.map(e => <Event event={e}></Event> ) }
+      <button onClick={handleAllEvents}> Add All</button>
+      {fakedata.map((e) => (
+        <Event event={e}></Event>
+      ))}
     </div>
   );
 };
