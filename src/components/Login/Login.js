337aafac84b6b52c66ee0cb56c361d530732df5e
RUHUL AMIN <59334063+RUHUL6093@users.noreply.github.com>
2021-05-19 09:42:15 +0600

    google sign in done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,12 +2,35 @@ import React from "react";
 import firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfiq from "./firebaseConfiq";
-firebase.initializeApp(firebaseConfiq);
+// firebase.initializeApp(firebaseConfiq);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfiq);
+}
 
 const Login = () => {
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+  const handleGooogleSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(googleProvider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        console.log(errorMessage)
+      });
+  };
   return (
-    <div>
-      <h1>This is destination</h1>
+    <div className="container m-5">
+      <button
+        onClick={handleGooogleSignIn}
+        className="btn btn-primary text-center"
+      >
+        {" "}
+        Continue With Google
+      </button>
     </div>
   );
 };
